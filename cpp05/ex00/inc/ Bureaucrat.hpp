9d3b86ee8885460f9f241abcd3a8ab6c6ad2a5e6
theeunsu <theeunsu@gmail.com>
2024-11-27 23:56:19 +0100

    ex00 tbc

---
+++ cpp05/ex00/inc/ Bureaucrat.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    Bureaucrat.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 23:55:14 by eahn              #+#    #+#             */
+/*   Updated: 2024/11/27 23:56:08 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class Bureaucrat 
+{
+	private:
+		const std::string name;
+		int grade;
+
+	public:
+}
