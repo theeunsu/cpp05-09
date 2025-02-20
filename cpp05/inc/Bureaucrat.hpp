7d3808e6eb422c0cbd307bb48c8a568fe1ca17d5
theeunsu <theeunsu@gmail.com>
2025-02-20 15:53:43 +0100

    header

--- cpp05/inc/Bureaucrat.hpp
+++ cpp05/inc/Bureaucrat.hpp
@@ -6,21 +6,53 @@
 /*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 16:35:41 by eahn              #+#    #+#             */
-/*   Updated: 2024/11/25 16:45:01 by eahn             ###   ########.fr       */
+/*   Updated: 2025/02/20 15:51:55 by eahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
-class Bureaucrat
+class Bureaucrat 
 {
-    private:
-        
+	private:
+		const std::string name;
+		int grade;
 
+	public:
+		// OCF
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade); 
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		// Getter
+		std::string getName() const;
+		int getGrade() const;
+
+		// Grade control
+		void incrementGrade();
+		void decrementGrade();
+
+		// Exeption classes (no need for OCF)
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLawException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
 };
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
 
 #endif
