5d34bae7756c77d458aff264587e0e6cb3200e35
theeunsu <theeunsu@gmail.com>
2025-03-26 18:10:55 +0100

    ex01 tbc

---
+++ cpp08/ex01/inc/Span.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 15:37:30 by eahn              #+#    #+#             */
+/*   Updated: 2025/03/26 18:08:03 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <iterator> 
+
+class Span
+{
+    private:
+        unsigned int _maxSize; // maximum number of elements
+        std::vector<int> _numbers; // container to store the numbers
+
+    public:
+        Span();
+        Span(unsigned int n);
+        Span(const Span &other);
+        Span &operator=(const Span &other);
+        ~Span();
+
+        void addNumber(int n);
+        int shortestSpan() const;
+        int longestSpan() const;
+
+
+};
+
+
+#endif
