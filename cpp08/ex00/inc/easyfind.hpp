60fad296f4bd6303d2d7c33e5b5154da32a55c31
theeunsu <theeunsu@gmail.com>
2025-03-26 15:34:35 +0100

    cpp08 ex00

--- cpp08/ex00/inc/easyfind.hpp
+++ cpp08/ex00/inc/easyfind.hpp
@@ -6,7 +6,23 @@
 /*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 17:01:24 by eahn              #+#    #+#             */
-/*   Updated: 2025/03/17 17:01:30 by eahn             ###   ########.fr       */
+/*   Updated: 2025/03/26 15:19:23 by eahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <algorithm> // std::find
+#include <stdexcept> // std::runtime_error
+
+template<typename T>
+typename T::iterator easyfind(T& container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw std::runtime_error("Value not found in container");
+    return it;
+}
+
+#endif
--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -6,7 +6,57 @@
 /*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 17:01:38 by eahn              #+#    #+#             */
-/*   Updated: 2025/03/17 17:01:39 by eahn             ###   ########.fr       */
+/*   Updated: 2025/03/26 15:34:09 by eahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/easyfind.hpp"
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+int main()
+{
+    // Test1: vector
+    std::vector<int> vec;
+    for (int i = 0; i < 10 ; ++i)
+        vec.push_back(i * 2); // 0, 2, 4, ..., 18 | push_back: to add values to container
+    
+    try
+    {
+        std::vector<int>::iterator it = easyfind(vec, 6);
+        std::cout << "Found in vector: " << *it << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Vector: " << e.what() << std::endl;
+    }
+        
+    // Test2: list
+    std::list<int> lst;
+    for (int i = 0; i < 5; ++i)
+        lst.push_back(i + 10); // 10, 11, 12, 13, 14
+    
+    try 
+    {
+        std::list<int>::iterator it = easyfind(lst, 12);
+        std::cout << "Found in list: " << *it << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "List: " << e.what() << std::endl;
+    }
+
+    // not found
+    try
+    {
+        easyfind(vec, 100);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Not found: " << e.what() << std::endl;
+    }
+    
+    return 0;
+}
